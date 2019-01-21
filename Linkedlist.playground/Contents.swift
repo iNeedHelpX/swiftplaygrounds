dc45e2f7c57658a9c00050ee0e792345f0e86d84
ineedhelpx <41366455+ineedhelpx@users.noreply.github.com>
2019-01-21 00:34:05 -0500

    Add files via upload

---
+++ Linkedlist.playground/Contents.swift
@@ -0,0 +1,63 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+class Node {
+    let value: Int
+    var next: Node?
+    
+    init(value: Int, next: Node?) {
+        self.value = value
+        self.next = next
+    }
+}
+
+
+
+class LinkedList {
+    var head: Node?
+    
+    func displayListItems() {
+        print ("items in the list:")
+        var current = head
+        while current != nil {
+            print(current?.value ?? "")
+            current = current?.next
+        }
+    }
+    //function to insert value into list
+    func insert(value: Int) {
+        //empty list case
+        if head == nil {
+            head = Node(value: value, next: nil)
+            return
+        }
+        var current = head
+        while current?.next != nil {
+            current = current?.next
+        }
+        current?.next = Node(value: value, next: nil)
+    }
+    
+    //delete function. Delete item in linked list
+    func deleteItem(value:Int) {
+        if head?.value == value {
+            head = head?.next
+        }
+        
+        var previous: Node?
+        var current = head
+        
+        while current != nil && current?.value != value {
+            previous = current
+            current = current?.next
+        }
+        previous?.next = current?.next
+    }
+
+}
+let myList = LinkedList()
+myList.insert(value: 5)
+myList.insert(value: 8)
+myList.insert(value: 6)
+myList.deleteItem(value: 8)
+myList.displayListItems()
